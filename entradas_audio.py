092d780c87e781d409664572d8a6f9d8acbe7733
100383397 <100383397@alumnos.uc3m.es>
2023-02-16 17:40:23 +0100

    #

--- entradas_audio.py
+++ entradas_audio.py
@@ -3,7 +3,10 @@ from mingus.midi import fluidsynth
 from mingus.containers import Note
 
 note_sequences = {}
-note_sequences['scale'] = ['C-5', 'C#-5', 'D-5', 'D#-5', 'E-5', 'F-5','F#-5', 'G-5','G#-5', 'A-5', 'A#-5','B-5']
+note_sequences['one_note'] = ['C-5']
+note_sequences['two_notes'] = ['E-5','G-5']
+note_sequences['scale1'] = [ 'C-5','D-5','E-5','F-5','G-5','A-5','B-5']
+note_sequences['scale2'] = [ 'C#-5','D#-5','F#-5','G#-5', 'A#-5',]
 separations_seconds = [0.5, 1, 2]
 
 fluidsynth.init(sf2='FluidR3 GM2-2.SF2')
@@ -15,8 +18,7 @@ def play_sequence(sequence, separation_seconds):
         fluidsynth.midi.sleep(seconds=separation_seconds)
         fluidsynth.stop_Note(note)
 
-# Genero audios y los guardo metiendo como entrada las notas de la escala con
-#distintas duraciones
+# Genero audios y los guardo metiendo como entrada las notas con distintas duraciones
 
 temp_n = 1
 for sequence in note_sequences:
@@ -28,7 +30,7 @@ for sequence in note_sequences:
 
         final_filename = \
             'audio_inputs/%s_%.1f_s.wav' % (sequence, separation_seconds)
-        # drop right channel, downmix to 10 kHz
+        # colocacion en el canal derecho y mezcla a 10 kHz 
         os.system("sox %s %s remix 1 rate 10000" % \
             (temp_filename, final_filename))
         temp_n += 1
