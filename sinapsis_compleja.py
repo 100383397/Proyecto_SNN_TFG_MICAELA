2ca7776482a1b4f6843040697cc9152c9b9bed3a
100383397 <100383397@alumnos.uc3m.es>
2023-02-18 13:25:40 +0100

    sinapsis mas compleja

---
+++ sinapsis_compleja.py
@@ -0,0 +1,32 @@
+import numpy as np
+import brian2 as b2
+import matplotlib.pyplot as plt
+
+
+N = 10
+G = b2.NeuronGroup(N, 'v:1')
+S = b2.Synapses(G, G)
+S.connect(condition='i!=j', p=0.2)
+
+def visualise_connectivity(S):
+    Ns = len(S.source)
+    Nt = len(S.target)
+    plt.figure(figsize=(10, 4))
+    plt.subplot(121)
+    plt.plot(np.zeros(Ns), np.arange(Ns), 'ok', ms=10)
+    plt.plot(np.ones(Nt), np.arange(Nt), 'ok', ms=10)
+    for i, j in zip(S.i, S.j):
+        plt.plot([0, 1], [i, j], '-k')
+    plt.xticks([0, 1], ['Source', 'Target'])
+    plt.ylabel('Neuron index')
+    plt.xlim(-0.1, 1.1)
+    plt.ylim(-1, max(Ns, Nt))
+    plt.subplot(122)
+    plt.plot(S.i, S.j, 'ok')
+    plt.xlim(-1, Ns)
+    plt.ylim(-1, Nt)
+    plt.xlabel('Source neuron index')
+    plt.ylabel('Target neuron index')
+    plt.savefig('SNN_figures/sinapsis_compleja.png' )
+
+visualise_connectivity(S)
