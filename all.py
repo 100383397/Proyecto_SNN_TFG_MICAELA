0192742b72c6cef7052042df915dc027f60579aa
100383397 <100383397@alumnos.uc3m.es>
2023-02-28 23:30:36 +0100

    #SNN structure mas explicada

--- all.py
+++ all.py
@@ -13,20 +13,30 @@ import modules.neurons as neuron_mod
 
 #b2.set_device('cpp_standalone')
 
-##################################################################
-##################################################################
-#parametros
+###########################################################################
+###########################################################################
+# Elproceso de integracion de las neuronas LIF es una especie de tira y afloja
+# de potenciales electricos. El punto clave es que este proceso refleja la
+# fuerza relativa excitatoria vs inhibitoria. Si la excitacion es mas fuerte
+# que la inhibicion, el potencial electrico de la neurona aumenta tal vez 
+# hasta el punto de superar el umbral y disparar un potencial de acción de 
+# salida. Si la inhibición es más fuerte, entonces el potencial eléctrico de 
+# la neurona disminuye, y así se aleja más de superar el umbral para disparar.
+##########################################################################
+##########################################################################
+
+#parametros usados para: neuronas, conexiones, monitores y ejecucion
 
 neuron_params = {}
 
-neuron_params['v_rest_e'] = -65 * b2.mV
-neuron_params['v_rest_i'] = -60 * b2.mV
-neuron_params['v_reset_e'] = -65 * b2.mV
-neuron_params['v_reset_i'] = -45 * b2.mV
-neuron_params['v_thresh_e'] = -52 * b2.mV
-neuron_params['v_thresh_i'] = -40 * b2.mV
-neuron_params['refrac_e'] = 5 * b2.ms
-neuron_params['refrac_i'] = 2 * b2.ms
+neuron_params['v_rest_e'] = -65 * b2.mV #potencial de reposo excitatoria
+neuron_params['v_rest_i'] = -60 * b2.mV #potencial de reposo inhibitoria
+neuron_params['v_reset_e'] = -65 * b2.mV #potencial de reset E
+neuron_params['v_reset_i'] = -45 * b2.mV #potencial de reset I
+neuron_params['v_thresh_e'] = -52 * b2.mV #umbral E
+neuron_params['v_thresh_i'] = -40 * b2.mV #umbral I
+neuron_params['refrac_e'] = 5 * b2.ms #periodo refractario E
+neuron_params['refrac_i'] = 2 * b2.ms #periodo refractario I
 neuron_params['vis'] = False
 neuron_params['tc_v_ex'] = 100 * b2.ms
 neuron_params['tc_v_in'] = 10 * b2.ms
@@ -53,7 +63,7 @@ connection_params['tc_post_ee'] = 20 * b2.ms
 connection_params['nu_ee_pre'] = 0.0001
 connection_params['nu_ee_post'] = 0.02
 connection_params['exp_ee_pre'] = 0.2
-connection_params['exp_ee_post'] = connection_params['exp_ee_pre']
+connection_params['exp_ee_post'] = 0.2
 connection_params['wmax_ee'] = 1.0
 connection_params['pre_w_decrease'] = 0.00025
 connection_params['tc_ge'] = 1 * b2.ms
@@ -66,15 +76,11 @@ connection_params['in-ex-w'] = 17.0
 
 run_params = {}
 
-run_params['layer_n_neurons'] = 16
+run_params['layer_n_neurons'] = 16 #numero de neuronas en la capa 1
 run_params['input_spikes_filename'] = 'spikes_inputs/two_notes_1.0_s.pickle'
 run_params['no_standalone'] = True
 # if args.run_time is not None:
 #     run_params['run_time'] = float(args.run_time) * b2.second
-run_params['save_results'] = False
-run_params['test_neurons'] = False
-run_params['test_stdp_curve'] = False
-run_params['test_competition'] = False
 
 monitor_params = {}
 monitor_params['monitors_dt'] = 1000/60.0 * b2.ms
@@ -84,10 +90,11 @@ params = (neuron_params, connection_params, monitor_params, run_params)
 #pickle
 #cpp standalone
 
+############################################################################
+
 def load_input(run_params):
-    """
-    Load spikes to be used for input neuroneuron_mod.
-    """
+    
+    #Cargamos los spikes para usarlos como entrada neuronal 
 
     pickle_filename = run_params['input_spikes_filename']
     with open(pickle_filename, 'rb') as pickle_file:
@@ -100,46 +107,101 @@ def load_input(run_params):
 
     return spikes
 
+###############################################################################
+
 def init_neurons(input_spikes, layer_n_neurons, neuron_params):
-    """
-    Initialise neurons.
-    """
+    
+    #Inicializamos las neuronas
+
     neurons = {}
 
-    n_inputs = 513
+    n_inputs = 513 #numero neuronas igual al de entradas_audio_spikes.py
     neurons['input'] = neuron_mod.prespecified_spike_neurons(
         n_neurons=n_inputs,
         spike_indices=input_spikes['indices'],
         spike_times=input_spikes['times']
     )
 
-    # excitatory neurons
+    # Neuronas excitatorias d ela capa 1
     neurons['layer1e'] = neuron_mod.excitatory_neurons(
         n_neurons=layer_n_neurons,
         params=neuron_params
     )
 
-    # inhibitory neurons
+    # Neuronas inhibitorias de la capa 1
     neurons['layer1i'] = neuron_mod.inhibitory_neurons(
         n_neurons=layer_n_neurons,
         params=neuron_params
     )
 
     # visualisation neurons
-    if neuron_params['vis']:
+    """if neuron_params['vis']:
         neurons['layer1vis'] = neuron_mod.visualisation_neurons(
             n_neurons=layer_n_neurons,
             params=neuron_params
-        )
+        )"""
 
     return neurons
 
+##########################################################################333
 
+def init_connections(neurons, connection_params):
+    
+    #Iniciamos las conexiones sinapticas entre diferentes capas de neuronas
+    
+    connections = {}
+
+    # input to layer 1 connections
+
+    source = neurons['input'] #la capa input de spikes del audio
+    target = neurons['layer1e'] #capa 1 de excitatorias
+    connections['input-layer1e'] = synapse_mod.stdp_ex_synapses(
+        source=source,
+        target=target,
+        connectivity=True, # all-to-all connectivity
+        params=connection_params
+    )
+
+    #asignamos pesos para la capa excitatoria 1
+
+    connections['input-layer1e'].w = 'rand() * 0.4'
+    weights = np.array(connections['input-layer1e'].w)
+    with open('input-layer1e-weights.pickle', 'wb') as pickle_file:
+        pickle.dump(weights, pickle_file)
+
+    # conexion excitatory to inhibitory
+    connections['layer1e-layer1i'] = synapse_mod.nonplastic_synapses(
+        source=neurons['layer1e'],
+        target=neurons['layer1i'],
+        connectivity='i == j',
+        synapse_type='excitatory'
+    )
+    connections['layer1e-layer1i'].w = connection_params['ex-in-w']
+
+    # conexion inhibitory to excitatory
+    connections['layer1i-layer1e'] = synapse_mod.nonplastic_synapses(
+        source=neurons['layer1i'],
+        target=neurons['layer1e'],
+        connectivity='i != j',
+        synapse_type='inhibitory'
+    )
+    connections['layer1i-layer1e'].w = connection_params['in-ex-w']
+
+    # excitatory to visualisation
+    """if 'layer1vis' in neurons:
+        connections['layer1e-layer1vis'] = synapse_mod.visualisation_synapses(
+            source=neurons['layer1e'],
+            target=neurons['layer1vis'],
+            connectivity='i == j',
+        )"""
+
+    return connections
+
+##################################################################################
 
 def init_monitors(neurons, connections, monitor_params):
-    """
-    Initialise Brian objects monitoring state variables in the network.
-    """
+    
+    #Inicializamos los objetos Brian monitoreando variables de estado en la red.
 
     monitors = {
         'spikes': {},
@@ -182,11 +244,12 @@ def init_monitors(neurons, connections, monitor_params):
 
     return monitors
 
+#########################################################################
+
 
 def run_simulation(run_params, neurons, connections, monitors):
-    """
-    Run the simulation using all the objects created so far.
-    """
+    
+    #Ejecutar la simulacion con los objetos creados hasta ahora
 
     net = b2.Network()
     for group in neurons:
@@ -201,67 +264,7 @@ def run_simulation(run_params, neurons, connections, monitors):
 
     return net
 
-
-def init_connections(neurons, connection_params):
-    """
-    Initialise synaptic connections between different layers of neurons.
-    """
-
-    connections = {}
-
-    # input to layer 1 connections
-
-    source = neurons['input']
-    target = neurons['layer1e']
-    connections['input-layer1e'] = synapse_mod.stdp_ex_synapses(
-        source=source,
-        target=target,
-        connectivity=True, # all-to-all connectivity
-        params=connection_params
-    )
-
-    # load saved weights, if they exist
-
-    if os.path.exists('input-layer1e-weights.pickle'):
-        with open('input-layer1e-weights.pickle', 'rb') as pickle_file:
-            pickle_obj = pickle.load(pickle_file)
-            connections['input-layer1e'].w = pickle_obj
-    else:
-        connections['input-layer1e'].w = 'rand() * 0.4'
-        weights = np.array(connections['input-layer1e'].w)
-        with open('input-layer1e-weights.pickle', 'wb') as pickle_file:
-            pickle.dump(weights, pickle_file)
-
-    # excitatory to inhibitory
-    connections['layer1e-layer1i'] = synapse_mod.nonplastic_synapses(
-        source=neurons['layer1e'],
-        target=neurons['layer1i'],
-        connectivity='i == j',
-        synapse_type='excitatory'
-    )
-    connections['layer1e-layer1i'].w = connection_params['ex-in-w']
-
-    # inhibitory to excitatory
-    connections['layer1i-layer1e'] = synapse_mod.nonplastic_synapses(
-        source=neurons['layer1i'],
-        target=neurons['layer1e'],
-        connectivity='i != j',
-        synapse_type='inhibitory'
-    )
-    connections['layer1i-layer1e'].w = connection_params['in-ex-w']
-
-    # excitatory to visualisation
-    if 'layer1vis' in neurons:
-        connections['layer1e-layer1vis'] = synapse_mod.visualisation_synapses(
-            source=neurons['layer1e'],
-            target=neurons['layer1vis'],
-            connectivity='i == j',
-        )
-
-    return connections
-
-
-
+#########################################################################
 
 
 spike_filename = os.path.basename(run_params['input_spikes_filename'])
@@ -270,6 +273,7 @@ run_id = spike_filename.replace('.pickle', '')
 #    param_mod.record_params(params, run_id)
 input_spikes = load_input(run_params)
 input_end_time = np.ceil(np.amax(input_spikes['times']))
+
 if 'run_time' not in run_params:
     run_params['run_time'] = input_end_time
 if not run_params['no_standalone']:
@@ -281,19 +285,12 @@ if not run_params['no_standalone']:
     b2.set_device('cpp_standalone', directory=build_dir)
 
 
-
-
 print("Initialising neurons...")
 neurons = init_neurons(
     input_spikes, run_params['layer_n_neurons'],
     neuron_params
 )
-
-##################################################################
-
-
-
-    
+print("done!")
 
 
 print("Initialising connections...")
@@ -302,11 +299,6 @@ connections = init_connections(
     connection_params
 )
 print("done!")
-
-    
-
-
-
 
 
 print("Initialising monitors...")
