e4ed2bd43128b2593fbf05ec620ac2d172def059
100383397 <100383397@alumnos.uc3m.es>
2023-06-08 16:57:35 +0200

    Script to run the validations

--- validation_script.sh
+++ validation_script.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-#PARA LA VALIDACION NO ES NECESARIO GUARDAR LOS PESOS DEL ENTRENAMIENTO, SE VALIDA CON UNOS PESOS 
-#CONGELADOS CONCRETOS, SE ESCOGE SOLO 1 ENTRENAMIENTO
-python_file="snn.py"
-folder_input_spikes="spikes_inputs_validation"
-folder_output_weights="weights_train"
 
+# Ejecutando este script se genera y se guarada en un archivo .txt las simulaciones de las validaciones
+# Para la validación no es necesario guardar los pesos, ya que se valida con los pesos congelados
+# del entrenamiento seleccionado, que es archivo weigths.pickle que aparece en el proyecto
+# en la carpeta principal.
 
-python3.9 "$python_file" "$folder_input_spikes/chords5_2.0_s.pickle" "$folder_output_weights/scale${i}rep_${j}iter_1.0_s.pickle" 2.0 >> "out_info.txt"
+python_file="snn.py" # Script que contiene la SNN
+folder_input_spikes="spikes_inputs_validation" #Carpeta de donde debe coger los .pickle como entrada a la red
+folder_output_weights="weights_train" #no se emplea
+
+# Hay que cambiar el nombre del archivo de los input spikes que se desee probar
+# Hay que modificar el parámetro final que indica la duración de nots del audio introducido, ya sea
+# 0.5, 1.0 o 2.0 
+
+python3.9 "$python_file" "$folder_input_spikes/chords0_0.5_s.pickle" "$folder_output_weights/scale${i}rep_${j}iter_1.0_s.pickle" 0.5 >> "out_info.txt"
